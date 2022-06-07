747cfafb445311caec710c63ee9944c26912adcd
Elvin <aliyeve669@gmail.com>
2022-06-07 16:23:12 +0400

    last version

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,6 +1,11 @@
 import logo from '../assets/images/logo.svg';
-
+let img_style={
+  width:'50px',
+  backgroundColor:'#3b82f6',
+  opacity:'0.9',
+  borderRadius:'10px'
+}
 const Logo = () => {
-  return <img src={logo} alt='jobster logo' className='logo' />;
+  return <img style={img_style} src={logo} alt='jobster logo' className='logo' />;
 };
 export default Logo;
